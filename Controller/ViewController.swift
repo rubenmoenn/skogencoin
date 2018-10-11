c86faaef3d6d759183e46f5deec00f55ffc57fa8
Ruben <wifiduck01@gmail.com>
2018-10-11 11:07:35 +0200

    First commit

---
+++ Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Skogen Coin
+//
+//  Created by Ruben Moen on 11/10/2018.
+//  Copyright © 2018 Skogen Coin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        registerButton.layer.cornerRadius = 2.4
+    }
+
+
+}
+
---
+++ RoundButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundButton.swift
+//  Skogen Coin
+//
+//  Created by Ruben Moen on 11/10/2018.
+//  Copyright © 2018 Skogen Coin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+
+}
